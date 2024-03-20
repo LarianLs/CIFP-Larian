ac543ae3021ae980c3970371f583bb4ede7eb19c
LarianLs <larianscerbet@gmail.com>
2024-03-20 13:56:02 +0000

    style updated

--- www/javascript/display-content.js
+++ www/javascript/display-content.js
@@ -3,33 +3,41 @@ function showOptions(type) {
     let options = '';
 
     switch (type) {
+        //centro
         case 'centro':
             options += '<a class="container-content" href="#">EVENTOS</a><br><hr class="hr">';
             options += '<a class="container-content" href="#">HORARIO</a><br><hr class="hr">';
             options += '<a class="container-content" href="#">NORMAS</a><br><hr class="hr">';
             options += '<a class="container-content" href="#">MUNICIPIO</a><br>';
             break;
+        // profesorado
         case 'profesorado':
-            options += '<a class="container-content" href="#">PERSONAL</a><br><hr class="hr">';
-            options += '<a class="container-content" href="#">PEDIR CITA</a><br>';
+            options += '<a class="container-content" href="professorship/staff.html">PERSONAL</a><br><hr class="hr">';
+            options += '<a class="container-content" href="professorship/dates.html">PEDIR CITA</a><br>';
             break;
+        case 'profesorado-2':
+            options += '<a class="container-content" href="../professorship/staff.html">PERSONAL</a><br><hr class="hr">';
+            options += '<a class="container-content" href="../professorship/dates.html">PEDIR CITA</a><br>';
+            break;
+        //alumnado
         case 'alumnado':
             options += '<a class="container-content" href="#">OBJETIVOS ACADÉMICOS</a><br><hr class="hr">';
             options += '<a class="container-content" href="#">ORIENTACIÓN ACADÉMICA</a><br><hr class="hr">';
             options += '<a class="container-content" href="#">PLANTILLA DE JUSTIFICANTE</a><br>';
             break;
+        //cursos
         case 'cursos':
             options += '<a class="container-content" href="courses/SMR1.html">SMR 1º</a><br><hr class="hr">';
             options += '<a class="container-content" href="courses/SMR2.html">SMR 2º</a><br><hr class="hr">';
             options += '<a class="container-content" href="courses/DAM1.html">DAM 1º</a><br><hr class="hr">';
             options += '<a class="container-content" href="courses/DAM2.html">DAM 2º</a>';
             break;
-            case 'cursos-2':
-                options += '<a class="container-content" href="SMR1.html">SMR 1º</a><br><hr class="hr">';
-                options += '<a class="container-content" href="SMR2.html">SMR 2º</a><br><hr class="hr">';
-                options += '<a class="container-content" href="DAM1.html">DAM 1º</a><br><hr class="hr">';
-                options += '<a class="container-content" href="DAM2.html">DAM 2º</a>';
-                break;
+        case 'cursos-2':
+            options += '<a class="container-content" href="SMR1.html">SMR 1º</a><br><hr class="hr">';
+            options += '<a class="container-content" href="SMR2.html">SMR 2º</a><br><hr class="hr">';
+            options += '<a class="container-content" href="DAM1.html">DAM 1º</a><br><hr class="hr">';
+            options += '<a class="container-content" href="DAM2.html">DAM 2º</a>';
+            break;
         default:
             break;
     }
